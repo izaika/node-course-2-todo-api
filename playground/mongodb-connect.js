385219009a8e8f5cd293203975807da8ed56adf6
Igor Zaika <izaika@Igors-MacBook-Pro.local>
2017-04-17 13:55:53 +0300

    init

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,33 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) =>  {
+	if (err) return console.log('Unable to connect to mongodb server');
+
+	console.log('Connected to mongodb server');
+
+	// db.collection('Todos').insertOne({
+	// 	text: 'Something to do',
+	// 	completed: false
+	// }, (err, result) =>  {
+	// 	if (err) return console.log('Unable to insert todo');
+	//
+	// 	console.log(JSON.stringify(result.ops, undefined, 2));
+	// });
+
+	// db.collection('Users').insertOne({
+	// 	name: 'Igor',
+	// 	age: 27,
+	// 	location: 'Sumy'
+	// }, (err, result) =>  {
+	// 	if (err) return console.log('Unable to insert user');
+	//
+	// 	console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
+	// });
+
+
+
+	db.close();
+});
