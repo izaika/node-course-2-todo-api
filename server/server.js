915520ff5c98bf41c150f40deb33586ed167d618
Igor Zaika <izaika@Igors-MacBook-Pro.local>
2017-04-17 23:24:17 +0300

    GET /todos/:id route was added

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+// const {mongoose} = require('./db/mongoose');
+// var {Todo} = require('./models/todo');
+// var {User} = require('./models/user');
+// const {ObjectID} = require('mongodb');
+
+const mongoose = require('./db/mongoose').mongoose;
+var Todo = require('./models/todo').Todo;
+var User = require('./models/user').User;
+const ObjectID = require('mongodb').ObjectID;
 
 var app = express();
 
@@ -26,6 +32,20 @@ app.get('/todos', (req, res) => {
 	}, (e) => {
 		res.status(400).send(e);
 	});
+});
+
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	if (!ObjectID.isValid(id)) return res.status(404).send('404. Record not found.');
+
+	Todo.findById(id).then((todo) => {
+		if (!todo) return res.status(404).send('404. Record not found.');
+
+		res.send({todo});
+	}, (e) => {
+		res.status(400).send('400. Bad request.');
+	})
 });
 
 
