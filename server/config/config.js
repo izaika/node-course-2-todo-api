1e912a528761fc5cb22cf27e5202adb201eea10b
Igor Zaika <izaika@Igors-MacBook-Pro.local>
2017-04-23 16:36:37 +0300

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,10 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-	process.env.PORT = 3000;
-	process.env.MONGODB_URI = 'mongodb://todo:123@ds135029.mlab.com:35029/node-todo-api';
-} else if (env === 'test') {
-	process.env.PORT = 3000;
-	process.env.MONGODB_URI = 'mongodb://todo:123@ds111441.mlab.com:11441/node-todo-api-test';
-} else if (env === 'production') {
-	process.env.MONGODB_URI = 'mongodb://todo:123@ds135029.mlab.com:35029/node-todo-api';
+if (env === 'development' || env === 'test') {
+	var config = require('./config.json');
+	var envConfig = config[env];
+
+	Object.keys(envConfig).forEach((key) => {
+		process.env[key] = envConfig[key];
+	});
 }
