bbdae16962e7225c48f6c48dea6e12984ef2e0c5
Igor Zaika <izaika@Igors-MacBook-Pro.local>
2017-04-23 14:57:59 +0300

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -65,6 +65,16 @@ UserSchema.methods.generateAuthToken = function () {
 	return user.save().then(() => token);
 };
 
+UserSchema.methods.removeToken = function (token) {
+	var user = this;
+
+	return user.update({
+		$pull: {
+			tokens: {token}
+		}
+	});
+};
+
 UserSchema.statics.findByToken = function (token) {
 	var User = this;
 	var decoded;
--- server/server.js
+++ server/server.js
@@ -112,7 +112,6 @@ app.get('/users/me', authenticate, (req, res) => {
 });
 
 
-
 app.post('/users/login', (req, res) => {
 	var body = _.pick(req.body, ['email', 'password']);
 	User.findByCredentials(body.email, body.password).then((user) => {
@@ -121,6 +120,15 @@ app.post('/users/login', (req, res) => {
 		});
 	}).catch((e) => {
 		res.status(400).send(e);
+	});
+});
+
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+	req.user.removeToken(req.token).then(() => {
+		res.status(200).send();
+	}, () => {
+		res.status(400).send();
 	});
 });
 
